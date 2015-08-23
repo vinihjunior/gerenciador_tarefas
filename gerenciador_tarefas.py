130dc1cfe90163b6f92cf297e658f77111fa5969
vinihjunior <devsoftawareengineering@gmail.com>
2015-08-23 00:56:37 -0300

    Primeira versão do app

---
+++ gerenciador_tarefas.py
@@ -0,0 +1,44 @@
+arquivo = "tarefas.txt"
+
+def nova_tarefa(texto):
+    try:
+        tarefas = open(arquivo,"a+")
+        tarefas.writelines(texto)
+        tarefas.close()
+        print("Tarefa salva com suceso!")
+    except IOError:
+        print("Erro ao criar nova Tarefa")
+
+
+def mostrar_tarefas():
+    try:
+        tarefas = open(arquivo,"r+")
+        print( '\n'+tarefas.read() )
+        tarefas.close()
+    except IOError:
+        print('\nArquivo não encontrado!')
+
+
+while(True):
+    print("************************************************")
+    print("*            Em que posso ajudar?              *")
+    print("*         1- Criar nova tarefa                 *")
+    print("*         2- Exibir tarefas salvas             *")
+    print("*         0- Sair                              *")
+    print("************************************************")
+
+    op = int(input("Digite sua opcao: "))
+
+    if op == 1:
+        new_task = input("Digite sua tarefa: ")
+        start_date = input("Data início: ")
+        end_date = input("Data final: ")
+        prioridade = input("Digite a prioridade de 0 á 10: ")
+
+        tarefa = ('| '+new_task+' | ' + start_date+' | ' + end_date+' | ' + prioridade+ ' |')
+        nova_tarefa(str(tarefa))
+    elif op == 2:
+        mostrar_tarefas()
+
+    elif op == 0:
+        break
